77195b21dd581908e12ebb0c5f740cceb7402c9e
Ethan Sherbondy <sherbondye@gmail.com>
2013-07-26 15:43:37 -0500

    inital unbound gui

--- project.clj
+++ project.clj
@@ -1,6 +1,19 @@
 (defproject zoe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Frame-by-frame animation in the web browser."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [core.async "0.1.0-SNAPSHOT"]
+                 [prismatic/dommy "0.1.1"]]
+
+  :plugins [[lein-cljsbuild "0.3.2"]]
+
+  :cljsbuild
+  {:builds
+   [{:id "simple"
+     :source-paths ["src"]
+     :compiler {:optimizations :simple
+                :pretty-print true
+                :output-to "zoe.js"}}]}
+  )
